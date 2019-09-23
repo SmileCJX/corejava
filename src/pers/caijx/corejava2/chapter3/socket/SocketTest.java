a1e3fcbf1b95a0e308c28c4b46bfedf5dac33b21
SmileCJX <1092925421@qq.com>
2019-09-23 22:44:33 +0800

    连接到当日时间服务，并打印输出

---
+++ src/pers/caijx/corejava2/chapter3/socket/SocketTest.java
@@ -0,0 +1,29 @@
+package pers.caijx.corejava2.chapter3.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @ClassName SocketTest
+ * @Description: 连接到当日时间服务，并打印输出
+ * @Author Think
+ * @Date 2019/9/23
+ * @Version V1.0
+ **/
+public class SocketTest {
+
+    public static void main(String[] args) throws IOException {
+        // 打开一个套接字
+        try (Socket s = new Socket("time-A.timefreq.bldrdoc.gov",13)){
+            InputStream inputStream = s.getInputStream();
+            Scanner in = new Scanner(inputStream);
+
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                System.out.println(line);
+            }
+        }
+    }
+}
